4764e5c453ae472ef1393906b5167b042079f683
Programmingwithshrey <112621849+Programmingwithshrey@users.noreply.github.com>
2023-12-14 21:15:05 +0800

    Create automate_sending.py

---
+++ automate_sending.py
@@ -0,0 +1,6 @@
+import datetime
+target_date_str = input("Enter the target date (YYYY-MM-DD): ")
+target_time_str = input("Enter the target time (HH:MM): ")
+
+target_date = datetime.datetime.strptime(f"{target_date_str} {target_time_str}", "%Y-%m-%d %H:%M")
+print(f"Target date and time set to: {target_date}")
