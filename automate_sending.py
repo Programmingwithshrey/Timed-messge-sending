1ebc5bd8b3d3e75fdd296e69de6cdfa71d49d27d
Programmingwithshrey <112621849+Programmingwithshrey@users.noreply.github.com>
2023-12-22 13:05:56 +0800

    Update automate_sending.py
    Variable name error/bug fixed...remove_str at end of variable

--- automate_sending.py
+++ automate_sending.py
@@ -1,8 +1,8 @@
 import datetime
-target_date_str = input("Enter the target date (YYYY-MM-DD): ")
-target_time_str = input("Enter the target time (HH:MM): ")
+target_date = input("Enter the target date (YYYY-MM-DD): ")
+target_time = input("Enter the target time (HH:MM): ")
 
-target_date = datetime.datetime.strptime(f"{target_date_str} {target_time_str}", "%Y-%m-%d %H:%M")
+target_date = datetime.datetime.strptime(f"{target_date} {target_time}", "%Y-%m-%d %H:%M")
 print(f"Target date and time set to: {target_date}")
 
 while True:
