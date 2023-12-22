1fbd93c4737579894095541446139c4919e459d2
Programmingwithshrey <112621849+Programmingwithshrey@users.noreply.github.com>
2023-12-22 13:50:30 +0800

    Update automate_sending.py
    Prints smtg at dedicated time and date

--- automate_sending.py
+++ automate_sending.py
@@ -1,11 +1,15 @@
 import datetime
+import pyautogui
 target_date = input("Enter the target date (YYYY-MM-DD): ")
 target_time = input("Enter the target time (HH:MM): ")
 
-target_date = datetime.datetime.strptime(f"{target_date} {target_time}", "%Y-%m-%d %H:%M")
-print(f"Target date and time set to: {target_date}")
+target_dt = str(datetime.datetime.strptime(f"{target_date} {target_time}", "%Y-%m-%d %H:%M"))
+print(f"Target date and time set to: {target_dt}")
+
+messagee = input("Whats ur message:")
 
 while True:
-    current_time = datetime.datetime.now()
-    if current_time == target_time:
-        print("current time now")
+    current_time = str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    if current_time == target_dt:
+        print("its time")
+        break
