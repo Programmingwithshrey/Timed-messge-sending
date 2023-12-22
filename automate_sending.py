f234f8a3b0f544237bde6635f21a0f8d8753318e
Programmingwithshrey <112621849+Programmingwithshrey@users.noreply.github.com>
2023-12-22 13:01:53 +0800

    Update automate_sending.py
    Prints message when time reached

--- automate_sending.py
+++ automate_sending.py
@@ -4,3 +4,8 @@ target_time_str = input("Enter the target time (HH:MM): ")
 
 target_date = datetime.datetime.strptime(f"{target_date_str} {target_time_str}", "%Y-%m-%d %H:%M")
 print(f"Target date and time set to: {target_date}")
+
+while True:
+    current_time = datetime.datetime.now()
+    if current_time == target_time:
+        print("current time now")
